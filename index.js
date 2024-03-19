ce45ef3da1a23257c366073c8e2642bbb51b4eef
hekerbaikcjr <127058700+hekerbaikcjr@users.noreply.github.com>
2024-03-19 11:08:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.set("view engine", "ejs");
 
 //Modify your URL here
-var hostURL="https://real-pea-coat-elk.cyclic.app";
+var hostURL="https://gray-alert-duckling.cyclic.app";
 //TOGGLE for Shorters
 var use1pt=false;
 
